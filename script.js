a6fa8141599dc79c4596682ae38e33e9bd1e083b
JAGADEEP10 <rjagadeep2004@gmail.com>
2024-11-10 19:32:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+WebFont.load({
+        google: {
+          families: ["Inter:100,200,300,regular,500,600,700,800,900"]
+        }
+      });
+
+
+! function(o, c) {
+        var n = c.documentElement,
+          t = " w-mod-";
+        n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+}(window, document);
+
+window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-131482533-1', {
+        'anonymize_ip': false
+});
